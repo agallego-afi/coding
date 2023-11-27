45edb8dbbe5b7286de05cbc57cbc28c2a8899713
manularrea <mlarrea286@soyudemedellin.edu.co>
2023-11-27 19:45:08 +0100

    Porposal refactoring

--- utils.py
+++ utils.py
@@ -1,12 +1,3 @@
-class Utils:
-  ratio = 9 / 5
-
-  @staticmethod
-  def convert(c):
-    '''
-      Converts the value
-      Parameters:
-        c - the value to convert
-        scientific: whether to convert to scientifi measurements
-    '''
-    return (c * Utils.ratio) + 32; # Convert
+def convert_to_a_scientific_format(number_to_convert):
+  ratio = 9/5
+  return (number_to_convert * ratio) + 32
