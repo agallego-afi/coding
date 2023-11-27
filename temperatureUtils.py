a4c54f2eb058b9a74e0a73287681b167e518982f
manularrea <mlarrea286@soyudemedellin.edu.co>
2023-11-27 20:27:27 +0100

    A better refactoring

--- temperatureUtils.py
+++ temperatureUtils.py
@@ -1,5 +1,6 @@
 class temperatureUtils
-def convert_celsius_to_farenheit(celsius):
   RATIO_FROM_CELSIUS_TO_FAHRENHEIT = 9/5
   FAHRENHEIT_OFFSET = 32
+
+def convert_celsius_to_farenheit(celsius):
   return ((celsius * temperatureUtils.RATIO_FROM_CELSIUS_TO_FAHRENHEIT) + temperatureUtils.FAHRENHEIT_OFFSET)
