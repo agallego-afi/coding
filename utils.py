5f19ed2d79113a568da0bd8ea88bc70e8786d805
manularrea <mlarrea286@soyudemedellin.edu.co>
2023-11-27 20:23:16 +0100

    A better refactoring

--- utils.py
+++ utils.py
@@ -1,2 +1,5 @@
-def convert_to_a_scientific_format(number_to_convert):
-  return (number_to_convert * (9/5)) + 32
+class temperatureUtils
+def convert_celsius_to_farenheit(celsius):
+  RATIO_FROM_CELSIUS_TO_FAHRENHEIT = 9/5
+  FAHRENHEIT_OFFSET = 32
+  return ((celsius * temperatureUtils.RATIO_FROM_CELSIUS_TO_FAHRENHEIT) + temperatureUtils.FAHRENHEIT_OFFSET)
